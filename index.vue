abf15321d1e9b6ac221e0aae10320dee79388628
caic <calvin519120063@gmail.com>
2026-07-03 14:40:19 +0800

    init portfolio

---
+++ index.vue
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>caich portfolio</title>
+</head>
+<body>
+  <h1>Hello, I'm caich</h1>
+  <p>This is my portfolio site (v1)</p>
+</body>
+</html>
